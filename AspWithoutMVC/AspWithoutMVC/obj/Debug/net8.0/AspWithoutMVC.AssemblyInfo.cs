a7084946121206550f91a69b1e32dbc949f050ce
Syed Shahzad <syed_shahzad@outlook.com>
2024-07-29 12:26:01 +0500

    Add New Files

--- AspWithoutMVC/AspWithoutMVC/obj/Debug/net8.0/AspWithoutMVC.AssemblyInfo.cs
+++ AspWithoutMVC/AspWithoutMVC/obj/Debug/net8.0/AspWithoutMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspWithoutMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f8299bea3380cd8f165672e0c8e54bb45a1c0f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspWithoutMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspWithoutMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ RoutingWithoutMVC/controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingWithoutMVC.controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Display()
+        {
+            return "Shahzad Display";
+        }
+
+        public int DisplayId(int Id)
+        {
+            return Id;
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+    }
+}
